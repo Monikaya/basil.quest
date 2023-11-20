25c433b0a59c5f700b82267c838d3430384cf323
Monikaya <64984313+Monikaya@users.noreply.github.com>
2023-11-19 16:15:46 -0800

    haha jk fixed version thing

--- components/AppBody.vue
+++ components/AppBody.vue
@@ -1,6 +1,12 @@
+<script setup>
+    const version = await getVersion();
+    console.log(version);
+</script>
+
 <template>
     <div class="app-body">
         <h1>basil.quest</h1>
+        <p>Version: {{ version }}</p>
     </div>
 </template>
 
---
+++ utils/getVersion.js
@@ -0,0 +1,23 @@
+// This file gets and returns the latest commit hash from the basil.quest git repo
+
+async function getDataFromGit(githubUrl) {
+    const latestCommit = await fetch(githubUrl)
+        .then((response) => response.json());
+    return latestCommit;
+}
+
+export default async function getVersion() {
+    // Generally static for now, is defined in variables to allow for future change
+    const user = "monikaya";
+    const repoName = "basil.quest";
+
+
+    const githubUrl = "https://api.github.com/repos/" + user + "/" + repoName + "/commits/master";
+    // await the json link, then actually assign latestCommit to the json data
+    const latestCommit = await getDataFromGit(githubUrl);
+    // Export the version, .substring being used to get only the first 5 characters of the hash
+    const version = latestCommit.sha.substring(0,5);
+    //console.log(version);
+    //.substring(0,5)
+    return version;
+}
