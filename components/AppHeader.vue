742f3c2b660001140bb9982298b1290f4aecf83b
Monikaya <64984313+Monikaya@users.noreply.github.com>
2023-12-03 00:29:16 -0800

    added dashboard button to header
    Added Dashboard component
    Migrated styles to a sheet

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,20 +1,20 @@
 <script setup lang="js">
-
+    import '~/assets/css/header.css';
 </script>
 
 <template>
     <div class="app-header">
-        <ULink to="/">basil.quest</ULink>
+        <div class="home-button">
+            <ULink to="/">basil.quest</ULink>
+        </div>
+        <div class="dashboard">
+            <ULink to="/admin">Dashboard</ULink>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    div {
-        text-align: left;
-        margin: 20px;
-    }
-    .app-header {
-        font-family: 'Roboto', sans-serif;
-        font-size: 22px;
-    }
+ /* I legit don't know why i need this
+ nuxt gets mad at me if I remove
+ gonna leave it like this & deal with it later :P */
 </style>
