b9059c2c0c19ecfd8f3ce0b44fc1b5a5d558945f
ahawker374 <ahawker374@gmail.com>
2023-12-19 06:31:56 +0000

    added signout, made signin/out buttons dynamic

--- pages/login.vue
+++ pages/login.vue
@@ -1,25 +1,44 @@
 
-<script>
-    import { GoogleAuthProvider } from '@firebase/auth'
-    export const googleAuthProvider = new GoogleAuthProvider();
-</script>
+<template>
+  <main>
+    <div class="titleSpace">
+      <button v-if="!user" @click="signinRedirect()">Sign In with Google</button>
+      <button v-if="user" @click="signMeOut()">Sign Out</button>
+    </div>
+  </main>
+</template>
 
 <script setup>
 import {
   getRedirectResult,
   signInWithRedirect,
   signOut,
+  getAuth
 } from 'firebase/auth'
 import { useCurrentUser, useFirebaseAuth } from 'vuefire'
 
-const auth = useFirebaseAuth() // only exists on client side
+// aren't using this rn bc it doesn't seem to work as well tbh. wompwomp
+// const auth = useFirebaseAuth()
+
+const auth = getAuth();
+const user = useCurrentUser()
+
+// sign out
+function signMeOut() {
+  signOut(auth).catch((reason) => {
+    console.error("Failed signOut", reason);
+    console.value = reason;
+  })
+}
 
 // display errors if any
 const error = ref(null)
 function signinRedirect() {
   signInWithRedirect(auth, googleAuthProvider).catch((reason) => {
-    console.error('Failed signinRedirect', reason)
-    error.value = reason
+    console.error('Failed signinRedirect', reason);
+    error.value = reason;
+  }).then((result) => {
+    const user = result.user;
   })
 }
 
@@ -32,10 +51,7 @@ onMounted(() => {
 })
 </script>
 
-<template>
-  <main>
-    <div class="titleSpace">
-      <button @click="signinRedirect()">SignIn with Google</button>
-    </div>
-  </main>
-</template>
+<script>
+    import { GoogleAuthProvider } from '@firebase/auth'
+    export const googleAuthProvider = new GoogleAuthProvider();
+</script>
