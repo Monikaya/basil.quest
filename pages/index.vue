b0aa9b297cb55fcfab86411fca52d6b5519a22e8
Monikaya <64984313+Monikaya@users.noreply.github.com>
2025-04-18 15:55:14 -0700

    awesome download page

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+        <div class="text-center w-full h-[6vh] border-b-3 justify-center flex items-center">
+            <h1 class="text-[24px]">i am a header</h1>
+        </div>
+        <div>
+            <h1 class="text-[50px] mt-[3vh]">
+                basil.quest
+            </h1>
+            <p>hello, welcome to the silly website</p>
+        </div>
+        <div>
+            <h2 class="text-center text-[30px] mt-[5vh]">About</h2>
+            <p>Hello! I'm Aubrey/Monika/etc (she/her), and I'm just a chilling trans woman who likes to do stuff.</p>
+        </div>
+        <div class="mt-[3vh] center">
+            <h1 class="text-[30px]">Webfish</h1>
+            <p class="w-[40vh] mx-auto">I host all sorts of servers, including one for Webfishing, Minecraft, and more in the future. Below is some information for anyone interested.</p>
+            <div class="flex justify-center items-center gap-x-[5vw]">
+                <ul class="list-disc text-[20px] text-left w-[40vh] self-start mt-[5vh]">
+                    <li class="mb-[1vh]">
+                        The purpose of this community is to be an 2SLGBTQIA+ safe space, hence the name "happy trans webfihsing"
+                    </li>
+                    <li class="mb-[1vh]">
+                        To the left is the Discord where you can contact us etc
+                    </li>
+                    <li class="mb-[1vh]">
+                        The webfishing server's code is <code>aubyay</code>
+                    </li>
+                    <li class="mb-[1vh]">
+                        Minecraft can be joined at <code>mc.basil.quest</code> and you can download the modpack
+                        <NuxtLink class="text-white" to="/modpack-download">here</NuxtLink>
+                    </li>
+                </ul>
+                <iframe class="block mt-[3vh]" src="https://discord.com/widget?id=1333124416149651507&theme=dark" width="350" height="380" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            </div>
+        </div>
+</template>
---
+++ pages/modpack-download.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full">
+    <h1 class="text-[30px] mt-[5vh]">Here you can download the Minecraft modpack files!</h1>
+    <p class="text-[18px]">When you're done, hop onto <code>mc.basil.quest</code></p>
+    <NuxtLink class="text-[10px]" to="/">Click me to go home</NuxtLink>
+  </div>
+  <div class="flex justify-center mt-[5vh] gap-x-[1vw]">
+    <div class="w-[20%] p-4 text-center">
+      <h2 class="text-[20px] mb-[1vh]">Prism Launcher</h2>
+      <a class="px-4 py-2 rounded buttoncustom" href="prism-modpack.zip" download > Download </a>
+    </div>
+    <div class="w-[20%] p-4 text-center">
+      <h2 class="text-[20px] mb-[1vh]">Modrinth</h2>
+      <a class="px-4 py-2 rounded buttoncustom" href="modrinth-modpack.zip" download > Download </a>
+    </div>
+    <div class="w-[20%] p-4 text-center">
+      <h2 class="text-[20px] mb-[1vh]">Curseforge</h2>
+      <a class="px-4 py-2 rounded buttoncustom" href="curseforge-modpack.zip" download > Download </a>
+    </div>
+  </div>
+</template>
