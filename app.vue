d51915b3a7c58e23f122367c516a78cdfa29c6a6
Monikaya <64984313+Monikaya@users.noreply.github.com>
2025-04-18 15:04:38 -0700

    erm kinda awesome

--- app.vue
+++ app.vue
@@ -1,13 +1,36 @@
 <template>
     <UApp>
-        <div class="text-lg text-center w-full h-[6vh] border-b-3 justify-center flex items-center">
-            <h1 class="text-[24px]">basil.quest</h1>
+        <div class="text-center w-full h-[6vh] border-b-3 justify-center flex items-center">
+            <h1 class="text-[24px]">i am a header</h1>
         </div>
         <div tag="">
             <h1 class="text-[50px] mt-[3vh]">
                 basil.quest
             </h1>
             <p>hello, welcome to the silly website</p>
+        </div>
+        <div>
+            <h2 class="text-center text-[30px] mt-[5vh]">About</h2>
+            <p>Hello! i'm Aubrey/Monika/etc (she/her), and I'm just a chilling trans woman who likes to do stuff.</p>
+        </div>
+        <div class="mt-[3vh] center">
+            <h1 class="text-[30px]">Webfish</h1>
+            <p class="w-[40vh] mx-auto">I host all sorts of servers, including one for Webfishing, Minecraft, and more in the future. Below are some resources for anyone interested.</p>
+            <div class="flex justify-center items-center gap-x-[5vw]">
+                <ul class="list-disc text-[20px] text-left w-[40vh] self-start mt-[5vh]">
+                    <li class="mb-[1vh]">
+                        Silly Discord where you can contact me etc
+                    </li>
+                    <li class="mb-[1vh]">
+                        The webfishing server's code is <code>aubyay</code>
+                    </li>
+                    <li class="mb-[1vh]">
+                        Minecraft can be joined at <code>mc.basil.quest</code> and you can download the modpack
+                        <a class="text-white" href="https://drive.google.com/drive/folders/1v4zZQ284eg2q1_td8z2v35d6MAkWGh1S?usp=sharing">here</a>
+                    </li>
+                </ul>
+                <iframe class="block mt-[3vh]" src="https://discord.com/widget?id=1333124416149651507&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            </div>
         </div>
     </UApp>
 </template>
