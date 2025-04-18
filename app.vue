43d41a597e1ac356002db21e452a93c77b134ca5
Monikaya <64984313+Monikaya@users.noreply.github.com>
2025-04-18 15:13:19 -0700

    i changed something

--- app.vue
+++ app.vue
@@ -11,15 +11,18 @@
         </div>
         <div>
             <h2 class="text-center text-[30px] mt-[5vh]">About</h2>
-            <p>Hello! i'm Aubrey/Monika/etc (she/her), and I'm just a chilling trans woman who likes to do stuff.</p>
+            <p>Hello! I'm Aubrey/Monika/etc (she/her), and I'm just a chilling trans woman who likes to do stuff.</p>
         </div>
         <div class="mt-[3vh] center">
             <h1 class="text-[30px]">Webfish</h1>
-            <p class="w-[40vh] mx-auto">I host all sorts of servers, including one for Webfishing, Minecraft, and more in the future. Below are some resources for anyone interested.</p>
+            <p class="w-[40vh] mx-auto">I host all sorts of servers, including one for Webfishing, Minecraft, and more in the future. Below is some information for anyone interested.</p>
             <div class="flex justify-center items-center gap-x-[5vw]">
                 <ul class="list-disc text-[20px] text-left w-[40vh] self-start mt-[5vh]">
                     <li class="mb-[1vh]">
-                        Silly Discord where you can contact me etc
+                        The purpose of this community is to be an 2SLGBTQIA+ safe space, hence the name "happy trans webfihsing"
+                    </li>
+                    <li class="mb-[1vh]">
+                        To the left is the Discord where you can contact us etc
                     </li>
                     <li class="mb-[1vh]">
                         The webfishing server's code is <code>aubyay</code>
@@ -29,7 +32,7 @@
                         <a class="text-white" href="https://drive.google.com/drive/folders/1v4zZQ284eg2q1_td8z2v35d6MAkWGh1S?usp=sharing">here</a>
                     </li>
                 </ul>
-                <iframe class="block mt-[3vh]" src="https://discord.com/widget?id=1333124416149651507&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                <iframe class="block mt-[3vh]" src="https://discord.com/widget?id=1333124416149651507&theme=dark" width="350" height="380" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
             </div>
         </div>
     </UApp>
